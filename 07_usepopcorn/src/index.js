12ad453881ce1fab66eb4c377e3ff3ee91248c7e
Kolling <gabrielgks10@gmail.com>
2024-05-16 11:55:56 -0300

    section 12 not completed

--- 07_usepopcorn/src/index.js
+++ 07_usepopcorn/src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<StarRating maxRating={10} />
+		<App />
+		{/* <StarRating maxRating={10} />
 		<StarRating
 			maxRating={5}
 			color="red"
 			size={24}
 			messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
 			defaultRating={3}
-		/>
+		/> */}
 	</React.StrictMode>
 );
